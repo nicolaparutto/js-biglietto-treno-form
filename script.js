27f8e0f5456adb1c49315be371b5d1fea5085f58
nicolaparutto <nicolaparutto2003@gmail.com>
2024-11-25 18:01:30 +0100

    feat: complete basic exercise with logic and operation & fixed file placement inside project folder

--- script.js
+++ script.js
@@ -1,23 +1,59 @@
-//elementi del form:
+//richiamo gli elementi del form:
 const passengerName = document.getElementById('name-input');
 const kmRoute = document.getElementById('km-input');
 const passengerAge = document.getElementById('age-select');
 const form = document.getElementById('form');
+const ticketContainer = document.querySelector('.ticket-container')
+//salvo i dati del form in delle variabili:
+const pageName = document.getElementById('name');
+const pageAge = document.getElementById('age');
+const pagekm = document.getElementById('route-km');
+const pageFinalPrice = document.getElementById('final-price');
 
-//elementi in cui salvare i dati:
-const name = document.getElementById('name');
-const age = document.getElementById('age');
-const km = document.getElementById('route-km');
-const pricefinal = document.getElementById('final-price');
-
+//dati utili per il calcolo dello sconto:
+const prezzoKm = 0.21;
+const discountMinorenne = 20;
+const discountOver = 40;
 
 
 
 form.addEventListener('submit', (send) => {
    send.preventDefault();
+   //salvo in due variabili la selezione dell'età e i km indicati da percorrere.
+   const age = passengerAge.value;
+   const km = kmRoute.value;
+   //calcolo del prezzo base:
+   const prezzoBase = km * prezzoKm;
+   //dichiaro una variabile che conterrà il prezzo finale:
+   let prezzoFinale;
 
-   name.innerText = passengerName.value.trim();
-   age.innerText = passengerAge.value;
-   km.innerText = `${kmRoute.value.trim()} Km`;
-   
+   //calcolo dello sconto sulle varie età
+   if (age == 'Minorenne'){
+      const sconto = prezzoBase * (discountMinorenne / 100);
+      prezzoFinale = prezzoBase - sconto;
+   } else if(age == 'Anziano'){
+      const sconto = prezzoBase * (discountOver / 100);
+      prezzoFinale = prezzoBase - sconto;
+   } else{
+      prezzoFinale = prezzoBase;
+   }
+   console.log(prezzoFinale.toFixed(2));
+
+   //stampo in pagina
+   pageName.innerText = passengerName.value;
+   pageAge.innerText = passengerAge.value;
+   pagekm.innerText = `${km} km`;
+   pageFinalPrice.innerText = prezzoFinale.toFixed(2);
+
+   //appare il biglietto in pagina
+   ticketContainer.classList.add('d-block');
+   //resetto gli il form
+   resetForm()
 })
+
+
+function resetForm(){
+   passengerName.value = '';
+   passengerAge.value = '';
+   kmRoute.value = '';
+}
